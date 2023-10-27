b997b254a69c32aedc90d73ae3bd0ce99d31504d
Marshawn Davidson <off2mxrs@gmail.com>
2023-10-27 12:13:25 -0700

    pod start / noise

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Head from 'next/head';
 import Layout, { siteTitle } from '../components/layout';
+import styles from '../components/layout.module.css';
 import utilStyles from '../styles/utils.module.css';
 import { getSortedPostsData } from '../lib/posts';
 import Link from 'next/link';
@@ -21,7 +22,7 @@ export default function Home({ allPostsData }) {
       <Head>
         <title>{siteTitle}</title>
       </Head>
-      <header id='intro' className={`${utilStyles.headingMd} ${utilStyles.portContainer}`}>
+      <header id='intro' className={`${utilStyles.headingMd} ${styles.pod} ${styles.noise}`}>
         <div>
           <p>lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.</p>
           <p>Ut enim ad minim ut labore et dolore.</p>
@@ -40,7 +41,7 @@ export default function Home({ allPostsData }) {
           <h1>mosaicmars</h1>
       </header>
 
-      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px} ${styles.pod} ${styles.noise}`}>
         <h2 id='work' className={utilStyles.headingLg}>work</h2>
         <ul className={utilStyles.list}>
           {allPostsData.map(({ id, date, title, image }) => (
@@ -65,15 +66,15 @@ export default function Home({ allPostsData }) {
         </ul>
       </section>
 
-      <section>
+      <section className={`${styles.pod}`}>
         <h2 id='resume' className={utilStyles.headingLg}>dev skills</h2>
       </section>
 
-      <section>
+      <section className={`${styles.pod}`}>
         <h2 id='inspo' className={utilStyles.headingLg}>inspo</h2>
       </section>
 
-      <section>
+      <section className={`${styles.pod}`}>
         <h2 id='contact' className={utilStyles.headingLg}>contact</h2>
       </section>
     </Layout>
