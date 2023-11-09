96d24209aaab5d9f96a423009260ce9ee6d64dde
Marshawn Davidson <off2mxrs@gmail.com>
2023-11-09 14:56:15 -0800

    deskName start

--- pages/index.js
+++ pages/index.js
@@ -32,25 +32,28 @@ export default function Home({ allPostsData }) {
             <div>
               <p>Ut enim ad minim ut labore et dolore.</p>
               <p>sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+              <h1 className={`${utilStyles.name} ${utilStyles.deskName}`}>{name}</h1>
             </div>
-            <div className={`${utilStyles.pfpSect}`}>
-              <Image
-                    priority
-                    src={pfp}
-                    className={`${utilStyles.pfp}`}
-                    placeholder='blur'
-                    sizes="100vw"
-                    alt={name}
-                    title={name}
-                    style={{
-                      height: 'auto',
-                      objectFit: 'cover',
-                      aspectRatio: '0.8',
-                      objectPosition: `55%`,
-                      // borderRadius: 75,
-                    }}
-                  />
-                <h1 className={`${utilStyles.name}`}>{name}</h1>
+            <div className={`${utilStyles.outerPfpSect}`}>
+              <div className={`${utilStyles.pfpSect}`}>
+                <Image
+                      priority
+                      src={pfp}
+                      className={`${utilStyles.pfp}`}
+                      placeholder='blur'
+                      sizes="100vw"
+                      alt={name}
+                      title={name}
+                      style={{
+                        height: 'auto',
+                        objectFit: 'cover',
+                        aspectRatio: '0.8',
+                        objectPosition: `55%`,
+                        // borderRadius: 75,
+                      }}
+                    />
+                  <h1 className={`${utilStyles.name} ${utilStyles.mobileName}`}>{name}</h1>
+                </div>
               </div>
             </div>
         </div>
