947674f32298f33d81e020b5fa4c00b8fbc18e44
Marshawn Davidson <off2mxrs@gmail.com>
2024-01-17 22:53:59 -0800

    show page edits 1

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -13,17 +13,22 @@ export default function Post({ postData }) {
       </Head>
       <article>
         <h1 className={utilStyles.headingLg}>{postData.title}</h1>
-        <div className={utilStyles.lightText}>
-        <p>{postData.date}</p>
+
+        <div className={utilStyles.workDetails}>
+          <small className={utilStyles.workLabels}>
+                <p>{postData.category}</p>
+                <p>{postData.date}</p>
+              </small>
+          <Image
+                  priority
+                  src={postData.image}
+                  className={''}
+                  height={400}
+                  width={400}
+                  alt=""
+                />
         </div>
-        <Image
-                priority
-                src={postData.image}
-                className={''}
-                height={400}
-                width={400}
-                alt=""
-              />
+       
         <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
       </article>
     </Layout>
